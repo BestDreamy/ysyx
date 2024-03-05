9053f56b2f8370ef3b276859585f0cc304784572
Baichuan Wei <3362484152@qq.com>
2024-03-05 17:09:23 +0800

    Start digital logic lab

---
+++ nvboard_tst/switch/csrc/switch.cc
@@ -0,0 +1,23 @@
+#include <nvboard.h>
+#include "Vswitch.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+static void single_cycle() {
+    dut.eval();
+}
+
+signed main() {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    while(true) {
+        nvboard_update();
+        single_cycle();
+    }
+
+    nvboard_quit();
+    return 0;
+}
