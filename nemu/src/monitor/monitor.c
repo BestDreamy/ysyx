a76309b9b6946b4a356dba39a2c2e007238ba3c3
Baichuan Wei <3362484152@qq.com>
2024-03-11 17:18:27 +0800

    update pa1

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -32,8 +32,8 @@ static void welcome() {
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NEMU!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
-  Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(0);
+  // Log("Exercise: Please remove me in the source code and compile NEMU again.");
+  // assert(0);
 }
 
 #ifndef CONFIG_TARGET_AM
@@ -133,7 +133,7 @@ void init_monitor(int argc, char *argv[]) {
     MUXDEF(CONFIG_ISA_x86,     "i686",
     MUXDEF(CONFIG_ISA_mips32,  "mipsel",
     MUXDEF(CONFIG_ISA_riscv,
-      MUXDEF(CONFIG_RV64,      "riscv64",
+    MUXDEF(CONFIG_RV64,        "riscv64",
                                "riscv32"),
                                "bad"))) "-pc-linux-gnu"
   ));
