cf4107c8369602c17d1cd2d0a5a28c3bc1b46afa
Baichuan Wei <3362484152@qq.com>
2024-04-04 23:09:27 +0800

    Update string and sprintf

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -7,27 +7,56 @@
 size_t strlen(const char *s) {
   size_t i = 0;
   for (; s[i]; i ++) ;
-  return i;
+  return i - 1;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i = 0;
+  while (*(src + i)) {
+    *(dst + i) = *(src + i);
+    i ++;
+  }
+  *(dst + i) = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  for (size_t = 0; i < n; i ++) {
+    if (*(src + i) == '\0') break;
+    *(dst + i) = *(src + i);
+  }
+  *(dst + i) = '\0';
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t base = strlen(dst), i = 0;
+  while (*(src + i)) {
+    *(dst + base + i) = *(src + i);
+    i ++;
+  }
+  *(dst + base + i) = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  size_t n = strlen(s1), m = strlen(s2), i = 0;
+  bool ok = (n == m);
+  while (*(s1 + i) && *(s2 + i) && ok) {
+    if (*(s1 + i) == *(s2 + i)) continue;
+    ok = 0;
+  }
+  return ok;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+int strncmp(const char *s1, const char *s2, size_t x) {
+  size_t n = strlen(s1), m = strlen(s2);
+  bool ok = (n >= x && m >= x);
+  for (size_t i = 0 ; i < x && ok; i ++) {
+    if (*(s1 + i) == *(s2 + i)) continue;
+    ok = 0;
+  }
+  return ok;
 }
 
 void *memset(void *s, int c, size_t n) {
