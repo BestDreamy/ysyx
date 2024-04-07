917afeb0abcc44ed0352d2c666f10c20a5692313
Baichuan Wei <3362484152@qq.com>
2024-04-07 22:43:20 +0800

    Start itrace, mtrace and ftrace

--- nemu/src/monitor/trace/trace.h
+++ nemu/src/monitor/trace/trace.h
@@ -5,8 +5,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-void itrace (uint32 inst);
+void itrace (uint64 pc, uint32 inst);
 void itraceDisplay();
 
 #endif
