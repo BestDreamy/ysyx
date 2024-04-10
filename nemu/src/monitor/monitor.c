d90dc7b9ccdd3b3f6c7fcaa7df95d6f9ea47cb12
Baichuan Wei <3362484152@qq.com>
2024-04-10 10:20:36 +0800

    Start itrace, mtrace and ftrace

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -15,10 +15,12 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
+#include <trace.h>
 
 void init_rand();
 void init_log(const char *log_file);
 void init_mem();
+void init_elf();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
@@ -44,6 +46,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -75,15 +78,17 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
+    {"elf"      , required_argument, NULL, 'e'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -91,6 +96,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE           read the symbol from .elf file\n");
         printf("\n");
         exit(0);
     }
@@ -112,6 +118,9 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize memory. */
   init_mem();
+
+  /* Initialize elf. */
+  init_elf();
 
   /* Initialize devices. */
   IFDEF(CONFIG_DEVICE, init_device());
--- nemu/src/monitor/trace/ftrace.c
+++ nemu/src/monitor/trace/ftrace.c
@@ -1,5 +1,13 @@
 #include <trace.h>
 
+void init_elf() {
+    ;
+}
+
+bool isSymbolFunc() {
+    return true;
+}
+
 void ftrace_call() {
     
     IFDEF(CONFIG_FTRACE, printf("call\n"));
