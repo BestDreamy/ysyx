be596aa886dfb91374818dc0bde00b178020ba79
Baichuan Wei <3362484152@qq.com>
2024-04-15 10:13:46 +0800

    NJU-ProjectN/abstract-machine ics2023 initialized
    NJU-ProjectN/abstract-machine c383159000a2975dabde43f221deae3d54bc48b0 am,npc: add uart config function

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -6,10 +6,10 @@ void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
-void __am_timer_rtc(AM_TIMER_RTC_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -18,6 +18,7 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
+  [AM_UART_CONFIG]  = __am_uart_config,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
