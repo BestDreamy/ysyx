0bab0692cd5b28c39aff6d12febfa5526208834d
Baichuan Wei <3362484152@qq.com>
2024-05-11 15:46:52 +0800

    npc for riscv32e

--- npc/csrc/cpu/cpu.cc
+++ npc/csrc/cpu/cpu.cc
@@ -50,26 +50,33 @@ void exec_once() {
 }
 
 void cpu_exec(uint64_t n) {
-    for (int i = 0; i < n - 1 && time_counter < FINISH_TIME; i ++) {
+    for (int i = 0; i < n - 1; i ++) {
         exec_once();
+
+        if (time_counter >= FINISH_TIME || npc_state.halt_ret) {
+            npc_state.state = NPC_END;
+            break;
+        }
+
+        if (npc_state.state != NPC_RUNNING) break;
+    }
 
     switch (npc_state.state) {
         case NPC_RUNNING: npc_state.state = NPC_STOP; break;
 
         case NPC_END: case NPC_ABORT:
 
-        IFDEF(CONFIG_MTRACE, mtraceDisplay());
+            IFDEF(CONFIG_MTRACE, mtraceDisplay());
 
-        IFDEF(CONFIG_ITRACE, itraceDisplay());
+            IFDEF(CONFIG_ITRACE, itraceDisplay());
 
-        Log("npc: %s at pc = " FMT_WORD,
-            (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", RED_TXT) :
-            (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", GREEN_TXT) :
-            ANSI_FMT("HIT BAD TRAP", RED_TXT))),
-            npc_state.halt_pc);
+            Log("npc: %s at pc = " FMT_WORD,
+                (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", RED_TXT) :
+                (npc_state.halt_ret ? ANSI_FMT("HIT GOOD TRAP", GREEN_TXT) :
+                ANSI_FMT("HIT BAD TRAP", RED_TXT))),
+                npc_state.halt_pc);
 
         // case NPC_QUIT: statistic();
-  }
     }
 }
 
