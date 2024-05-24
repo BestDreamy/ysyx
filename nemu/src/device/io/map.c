cd78d082536364bfb0b458741fbae88f74204a70
Baichuan Wei <3362484152@qq.com>
2024-05-24 22:55:25 +0800

    add dtrace

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -72,11 +72,9 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
 }
 
 void trace_dread(paddr_t addr, int len, IOMap *map) {
-	log_write("dtrace: read %10s at " FMT_PADDR ",%d\n",
-		map->name, addr, len);
+	log_write("dtrace: read %10s at " FMT_PADDR ",%d\n", map->name, addr, len);
 }
 
 void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map) {
-	log_write("dtrace: write %10s at " FMT_PADDR ",%d with " FMT_WORD "\n",
-		map->name, addr, len, data);
+	log_write("dtrace: write %10s at " FMT_PADDR ",%d with " FMT_WORD "\n", map->name, addr, len, data);
 }
