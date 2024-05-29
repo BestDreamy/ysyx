e37c709c1a7d5ae566310182d21b9b015d4a1e1b
Baichuan Wei <3362484152@qq.com>
2024-05-29 08:37:23 +0800

    add device to npc

--- npc/csrc/device/serial.cc
+++ npc/csrc/device/serial.cc
@@ -11,7 +11,8 @@
 static uint8_t *serial_base = NULL;
 
 static void serial_putc(char ch) {
-  MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
+  // -MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
+  putch(ch);
 }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
