07557c9d8eccad71d8fd95aae88479e2f3cea0b4
Baichuan Wei <3362484152@qq.com>
2024-06-16 22:51:45 +0800

    Finish pa3.3

---
+++ nanos-lite/src/irq.c
@@ -0,0 +1,16 @@
+#include <common.h>
+
+static Context* do_event(Event e, Context* c) {
+  switch (e.event) {
+    case EVENT_YIELD: printf("Event yield is triggered\n"); break;
+    case EVENT_SYSCALL: printf("Event syscall is triggered\n"); break;
+    default: panic("Unhandled event ID = %d", e.event);
+  }
+
+  return c;
+}
+
+void init_irq(void) {
+  Log("Initializing interrupt/exception handler...");
+  cte_init(do_event);
+}
