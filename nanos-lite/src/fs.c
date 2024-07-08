6a7f90c245c19e149bde28c2e3f09c378b2d3d6e
Baichuan Wei <3362484152@qq.com>
2024-07-08 23:00:50 +0800

    Finish pa3.4

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -13,6 +13,10 @@ typedef struct {
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
@@ -30,7 +34,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
-#define NR_FILES int(sizeof(file_table) / sizeof Finfo)
+#define NR_FILES (int)(sizeof(file_table) / sizeof(Finfo))
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
@@ -44,11 +48,11 @@ int fs_open(const char *pathname, int flags, int mode) {
   for (int i = 3; i < NR_FILES; i ++) {
     if (strcmp(file_table[i].name, pathname) == 0) {
       opening_offset = 0;
-      fa = i;
+      fd = i;
       break;
     }
   }
-  if (fd == -1) panic("No this file in FIEL_TABLE.")
+  if (fd == -1) panic("No this file in FIEL_TABLE.");
   return fd;
 }
 
@@ -64,8 +68,29 @@ size_t fs_read(int fd, void *buf, size_t len) {
   return len;
 }
 
-// size_t fs_write(int fd, const void *buf, size_t len);
-// size_t fs_lseek(int fd, size_t offset, int whence);
+size_t fs_write(int fd, const void *buf, size_t len) {
+  return 0;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  if (fd < 3) panic("Seek invalid fd.");
+
+  Finfo opening_file = file_table[fd];
+  switch (whence) {
+    case SEEK_SET:
+      opening_offset = offset;
+      break;
+    case SEEK_CUR:
+      opening_offset = opening_offset + offset;
+      break;
+    case SEEK_END:
+      opening_offset = opening_file.size + offset;
+      break;
+    default:
+      panic("Invalid whence when seeking.");
+  }
+  return opening_offset;
+}
 
 int fs_close(int fd) {
   return 0;
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,8 @@
-#include <proc.h>
 #include <elf.h>
+#include <proc.h>
 #include "ramdisk.h"
 #include "loader.h"
+#include "fs.h"
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -12,22 +13,51 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  /* Elf_Ehdr elf_header; */
-  /* ramdisk_read(&elf_header, 0, sizeof(elf_header)); */
-  /**/
-  /* // rv64-elf -a build/ramdisk.img */
-  /* assert(*(uint32_t *)elf_header.e_ident == 0x464C457f); */
-  /**/
-  /* Elf_Phdr elf_segment_arr[elf_header.e_phnum]; */
-  /* ramdisk_read(elf_segment_arr, elf_header.e_phoff, sizeof(Elf_Phdr) * elf_header.e_phnum); */
-  /* for (int i = 0; i < elf_header.e_phnum; i++) { */
-  /*   if (elf_segment_arr[i].p_type == PT_LOAD) { */
-  /*     ramdisk_read((void *)elf_segment_arr[i].p_vaddr, elf_segment_arr[i].p_offset, elf_segment_arr[i].p_memsz); */
-  /*     memset((void *)(elf_segment_arr[i].p_vaddr + elf_segment_arr[i].p_filesz), 0, elf_segment_arr[i].p_memsz - elf_segment_arr[i].p_filesz); */
-  /*   } */
-  /* } */
-  /**/
-  /* return elf_header.e_entry; */
+  // Elf_Ehdr elf_header;
+  // ramdisk_read(&elf_header, 0, sizeof(elf_header));
+
+  // // rv64-elf -a build/ramdisk.img
+  // assert(*(uint32_t *)elf_header.e_ident == 0x464C457f);
+
+  // Elf_Phdr elf_segment_arr[elf_header.e_phnum];
+  // ramdisk_read(elf_segment_arr, elf_header.e_phoff, sizeof(Elf_Phdr) * elf_header.e_phnum);
+  // for (int i = 0; i < elf_header.e_phnum; i++) {
+  //   if (elf_segment_arr[i].p_type == PT_LOAD) {
+  //     Elf64_Addr  p_vaddr  = elf_segment_arr[i].p_vaddr;
+  //     Elf64_Off   p_offset = elf_segment_arr[i].p_offset;
+  //     Elf64_Xword p_memsz  = elf_segment_arr[i].p_memsz;
+  //     Elf64_Xword p_filesz = elf_segment_arr[i].p_filesz;
+  //     ramdisk_read((void *)p_vaddr, p_offset, p_memsz);
+  //     memset((void *)(p_vaddr + p_filesz), 0, p_memsz - p_filesz);
+  //   }
+  // }
+
+  // return elf_header.e_entry;
+
+  Elf_Ehdr elf_header;
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, &elf_header, sizeof elf_header);
+
+  assert(*(uint32_t *)elf_header.e_ident == 0x464C457f);
+
+  Elf_Phdr elf_segment_arr[elf_header.e_phnum];
+  fs_lseek(fd, elf_header.e_phoff, SEEK_SET);
+  fs_read(fd, elf_segment_arr, sizeof(Elf_Phdr) * elf_header.e_phnum);
+  
+  for (int i = 0; i < elf_header.e_phnum; i++) {
+    if (elf_segment_arr[i].p_type == PT_LOAD) {
+      fs_lseek(fd, elf_segment_arr[i].p_offset, SEEK_SET);
+      // man elf
+      Elf64_Addr  p_vaddr  = elf_segment_arr[i].p_vaddr;
+      Elf64_Xword p_memsz  = elf_segment_arr[i].p_memsz;
+      Elf64_Xword p_filesz = elf_segment_arr[i].p_filesz;
+      fs_read(fd, (void *)(uintptr_t)p_vaddr, p_memsz);
+      memset((void *)(uintptr_t)(p_vaddr + p_filesz), 0, p_memsz - p_filesz);
+    }
+  }
+  fs_close(fd);
+
+  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
