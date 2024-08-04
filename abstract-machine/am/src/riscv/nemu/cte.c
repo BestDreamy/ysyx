6a270a0d28aba63e6ce760b818693664b82ed716
Baichuan Wei <3362484152@qq.com>
2024-08-04 16:29:53 +0800

    Start RT-Thread

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -41,6 +41,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context* c = (Context*)kstack.end - 1;
   c->mepc = (uintptr_t)entry; // wait for mret
+  c->GPR2 = (uintptr_t)arg;
   return c;
 }
 
