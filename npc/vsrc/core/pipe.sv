7b2078345eafcd9e316e1e46d420f9892ea92e71
Baichuan Wei <3362484152@qq.com>
2024-10-17 09:02:08 +0800

    Solve ctrl conflict for 4-pipe npc

--- npc/vsrc/core/pipe.sv
+++ npc/vsrc/core/pipe.sv
@@ -15,7 +15,7 @@ module pipe
 
     wire valid_en;
 
-    assign valid_en = (~valid_q & pin_valid);
+    assign valid_en = (~valid_q & pin_valid) | (valid_q & pout_ready);
 
     always @(posedge clk) begin
         if (rst == `ysyx_23060251_rst_enable)
