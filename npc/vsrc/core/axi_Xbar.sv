15f34f123f3bf1059249982f9dcbdfa920e4f4dd
Baichuan Wei <3362484152@qq.com>
2025-01-07 22:08:18 +0800

    Finish 5-pipe

--- npc/vsrc/core/axi_Xbar.sv
+++ npc/vsrc/core/axi_Xbar.sv
@@ -1,6 +1,15 @@
 module axi_Xbar (
-    axi_ar_if                   slv_ar,
-    axi_r_if                    slv
+    axi_ar_if.Slave                   axi_slv_ar,
+    axi_r_if.Slave                    axi_slv_r,
+    axi_aw_if.Slave                   axi_slv_aw,
+    axi_w_if.Slave                    axi_slv_w,
+    axi_b_if.Slave                    axi_slv_b,
+
+    axi_ar_if.Master                  axi_mst_ar,
+    axi_r_if.Master                   axi_mst_r,
+    axi_aw_if.Master                  axi_mst_aw,
+    axi_w_if.Master                   axi_mst_w,
+    axi_b_if.Master                   axi_mst_b
 );
 
 endmodule
